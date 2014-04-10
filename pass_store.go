a1d77bf6f7d573182485c8ef58a132a697efde03
Pablo Verdú Romero <panfri787@gmail.com>
2014-04-10 09:19:53 +0200

    Modificada la visibilidad de algunas funciones

--- pass_store.go
+++ pass_store.go
@@ -14,7 +14,7 @@ type Direccion struct {
 }
 
 //Comprueba si se ha producido error, añadiendo una entrada al log en caso afirmativo.
-func Check_error(desc string, err error) {
+func check_error(desc string, err error) {
 	if err != nil {
 		log.Fatal(desc, err)
 	}
@@ -22,7 +22,7 @@ func Check_error(desc string, err error) {
 
 //Almacena un nuevo password actualizando el directorio.
 func StorePass(user string, site string, pass string) {
-	dir := GetDirFile(user)
+	dir := getDirFile(user)
 	var mf *os.File
 	fileName := user + "_main"
 
@@ -32,33 +32,33 @@ func StorePass(user string, site string, pass string) {
 	}
 
 	mf, err := os.OpenFile(fileName, os.O_RDWR, 0666)
-	Check_error("opening main file to write", err)
+	check_error("opening main file to write", err)
 	defer mf.Close()
 
 	var d Direccion
 
 	stats, err := mf.Stat()
-	Check_error("retriving main file stats", err)
+	check_error("retriving main file stats", err)
 
 	d.Init = stats.Size()
 	mf.Seek(d.Init, 0)
 
 	writed, err := mf.Write([]byte(pass))
-	Check_error("writing main:", err)
+	check_error("writing main:", err)
 
 	d.Length = writed
 	dir[site] = d
 
-	WriteDirFile(user, dir)
+	writeDirFile(user, dir)
 }
 
 //Devuelve un pass concreto.
 func GetPass(user string, site string) string {
-	directory := GetDirFile(user)
+	directory := getDirFile(user)
 	dir := directory[site]
 
 	mf, err := os.Open(user + "_main")
-	Check_error("opening for reading main file", err)
+	check_error("opening for reading main file", err)
 
 	mf.Seek(dir.Init, 0)
 	b := make([]byte, dir.Length)
@@ -68,7 +68,7 @@ func GetPass(user string, site string) string {
 }
 
 //Devuelve el contenido del fichero de directorio del usuario.
-func GetDirFile(user string) map[string]Direccion {
+func getDirFile(user string) map[string]Direccion {
 	dirByte, err := ioutil.ReadFile(user + "_directory")
 
 	//Si el fichero no existe aun, devuelvo un directorio vacio.
@@ -82,13 +82,13 @@ func GetDirFile(user string) map[string]Direccion {
 }
 
 //Escribe el directorio actualizado en su fichero correspondiente.
-func WriteDirFile(user string, dir map[string]Direccion) {
+func writeDirFile(user string, dir map[string]Direccion) {
 	f, err := os.Create(user + "_directory")
-	Check_error("opening directory file", err)
+	check_error("opening directory file", err)
 	defer f.Close()
 
 	b, err := json.Marshal(dir)
-	Check_error("encoding dir", err)
+	check_error("encoding dir", err)
 
 	f.Write(b)
 }
