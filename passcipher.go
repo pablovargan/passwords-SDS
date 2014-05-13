3eb4dc1953e16266d4e68c9b25d69511ac0cc6fd
Pablo Verdú Romero <panfri787@gmail.com>
2014-05-13 15:04:30 +0200

    Implementada una base de la libreria para cifrar passwords

---
+++ passcipher.go
@@ -0,0 +1,50 @@
+package passcipher
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"io"
+	"passwords-SDS/passwords"
+)
+
+/*
+Cifra una cadena. Devuelve la cadena cifrada y el vector de inicializacion.
+*/
+func Cipher(plain_text []byte, cipher_pass []byte) []byte {
+	block, err := aes.NewCipher(cipher_pass)
+	if err != nil {
+		panic(err)
+	}
+
+	ciphertext := make([]byte, aes.BlockSize+len(plain_text))
+	iv := ciphertext[:aes.BlockSize]
+
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		panic(err)
+	}
+
+	stream := cipher.NewCTR(block, iv)
+	stream.XORKeyStream(plain_text, ciphertext[aes.BlockSize:])
+
+	return ciphertext
+}
+
+/*
+Descifra una cadena. Devuelve la cadena descifrada.
+*/
+func Decipher(ciphertext []byte, cipher_pass []byte) []byte {
+
+	//Falta validar que el password de cifrado sea correcto.
+
+	block, err := aes.NewCipher(cipher_pass)
+	if err != nil {
+		panic(err)
+	}
+
+	plain_text := make([]byte, len(ciphertext[aes.BlockSize:]))
+	stream := cipher.NewCTR(block, ciphertext[:aes.BlockSize])
+	stream.XORKeyStream(plain_text, ciphertext[aes.BlockSize:])
+
+	return plain_text
+}
