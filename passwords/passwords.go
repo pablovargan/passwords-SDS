fbf8434a3376330a8f2fd21965d15f3c713ee1d3
Pablo Verdú Romero <panfri787@gmail.com>
2014-05-22 12:39:14 +0200

    Añadido metodo que lista los sites almacenados en la libreria passwords. Refactorizado un metodo para obtener el almacen desde el fichero

--- passwords/passwords.go
+++ passwords/passwords.go
@@ -2,6 +2,7 @@ package passwords
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 )
@@ -42,11 +43,27 @@ func StorePassword(user string, site string, entry Password) {
 func GetPassword(user string, site string) Password {
 	var warehouse map[string]Password
 
+	warehouse = getWarehouse(user)
+	return warehouse[site]
+}
+
+func ListSites(user string) {
+	warehouse := getWarehouse(user)
+
+	for site, _ := range warehouse {
+		fmt.Println(site)
+	}
+}
+
+func getWarehouse(user string) map[string]Password {
+	var warehouse map[string]Password
+
 	bytes, err := ioutil.ReadFile(user)
 	if err != nil {
 		log.Fatal("The file does not exist")
 	}
 
 	json.Unmarshal(bytes, &warehouse)
-	return warehouse[site]
+
+	return warehouse
 }
